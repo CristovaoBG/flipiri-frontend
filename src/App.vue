8ea14b62ed361c7b123bc5c5b7de6780f340330e
Cristovao <cristovao@live.com>
2024-05-18 17:02:15 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import SheetViewer from './components/SheetViewer.vue';
+const dictionary = [
+          { id: 1, name: 'John Doe', email: 'john@example.com' },
+          { id: 2, name: 'Jane Smith', email: 'janeple.com' },
+          { id: 3, name: 'Sam Johnson', email: 'saxample.com' }
+        ]
+</script>
+
+<template>
+<SheetViewer :dictionary="dictionary">
+
+</SheetViewer>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/SheetViewer.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+      <h2>User Table</h2>
+      <table>
+        <thead>
+          <tr>
+            <th v-for="k in Object.keys(dictionary[0])" :key="k">{{ k }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in dictionary" :key="item">
+            <td v-for="k in Object.keys(item)" :key="k"> {{ item[k] }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+
+  export default {
+    name: 'UserTable',
+    props:[
+        'dictionary'
+    ],
+    data() {
+      return {
+        dictionar: [
+          { i: 1, name: 'John Doe', email: 'john@example.com' },
+          { i: 2, name: 'Jane Smith', email: 'janeple.com' },
+          { i: 3, name: 'Sam Johnson', email: 'saxample.com' }
+        ]
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  
+  th, td {
+    padding: 8px 12px;
+    border: 1px solid #ddd;
+    text-align: left;
+  }
+  
+  th {
+    background-color: #9b3f3f;
+  }
+  </style>
+  
