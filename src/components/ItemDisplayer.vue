45f7c59de52334cf3137d17c49f972b3cfb682ba
Cristovao <cristovao@live.com>
2024-05-21 22:09:06 -0300

    fetch value from ObjectId

--- src/components/ItemDisplayer.vue
+++ src/components/ItemDisplayer.vue
@@ -1,29 +1,47 @@
 <template>
-  <div v-if="typeof item === 'string'">
-    <div v-if="item.includes('ObjectId')">
-        ObjectId: {{ item }}
-    </div>
-    <div v-else>
-        String: {{ item }}
-    </div>
-  </div>
-  <div v-else-if="(item instanceof Array)">
-    ARRAY<br>
-    <div v-for="k in item" :key="k"> 
-        Sub-item: {{ k }}
-    </div> 
-  </div>
-  <div v-else>
-    Other type: {{ item }}
-  </div>
+    {{ value }}
 </template>
-
-<script>
-export default {
-    props: ['item']
-}
-</script>
-
-<style>
-
-</style>
+  
+  <script>
+  import axios from 'axios';
+  import { URL_BASE } from '../constants';
+  
+  export default {
+      props: ['item'],
+      data() {
+          return {
+              value: '',
+              isLoading: false
+          }
+      },
+      methods: {
+          async getObjId(item) {
+              this.isLoading = true;
+              try {
+                  const response = await axios.get(URL_BASE + 'test/', {
+                      params: {
+                        _id: item.split('\'')[1], // porque é tipo "ObjectData('valor')"
+                      },
+                  });
+                  this.value += response.data + '\n'; // Atualize o estado diretamente
+              } catch (error) {
+                  console.error('Erro ao fazer a requisição:', error);
+              } finally {
+                  this.isLoading = false;
+              }
+          }
+      },
+      mounted() {
+          if (typeof this.item === 'string' && this.item.includes('ObjectId')) {
+              this.getObjId(this.item);
+          } else if (Array.isArray(this.item)) {
+              this.item.forEach(k => {
+                  this.getObjId(k);
+              });
+          } else {
+            this.value = this.item
+          }
+      }
+  }
+  </script>
+  
